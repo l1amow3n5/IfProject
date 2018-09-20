88cc283e31d0077d0bd7d11c2b88a7e2728200bf
Liam Owen <l1amow3n5@yahoo.com>
2018-09-20 12:09:36 -0600

    Created data members section and added the constructures

---
+++ src/model/GolfProperties.java
@@ -0,0 +1,30 @@
+package model;
+
+public class GolfProperties
+{
+	//Data member section
+	//All numbers => 0
+	// boolean => false
+	//Objects => null
+	private int timeHours;  
+	private int timeMinutes; 
+	private int score;
+	private String name;
+	private double handicap;
+	private String golfCourse;
+	public GolfProperties() 
+	{
+		
+	}
+	public GolfProperties(String name, int timeHours, int timeMinutes, int score, double handicap,String golfCourse)
+	{
+		this.name = name;
+		this.timeHours = timeHours;
+		this.timeMinutes = timeMinutes;
+		this.score = score;
+		this.handicap = handicap;
+		this.golfCourse = golfCourse;
+	}
+	
+	
+}
